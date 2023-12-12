f834fb1d4d752ad2305950b6a3ad7bd1618b80a5
visnupiriyan <visnupiriyan.kumarraja01@student.csulb.edu>
2023-12-11 22:14:25 -0800

    SettingUpdated

--- visnupiriyan_kumaraja/visnupiriyan_kumaraja/settings.py
+++ visnupiriyan_kumaraja/visnupiriyan_kumaraja/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'visnupiriyan_kumaraja.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
